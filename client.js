46cd9cb0278dfc14a33ff860c94e703a459fc72f
Kristen Rebello <42438056+kris17ten@users.noreply.github.com>
2023-04-07 12:05:47 +0100

    Update client.js

--- client.js
+++ client.js
@@ -131,3 +131,24 @@ marathon = {
 	}
 };
 
+function readJSON(file) {
+    var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {//Called when data returns from server
+		if (this.readyState == 4 && this.status == 200) {
+			var marshie = JSON.parse(this.responseText);
+			alert(marshie);
+			//createUserHeader(marshie[1]);
+		}   
+		else {
+		  //alert("Error communicating with server: " + xhttp.status);
+		}
+	  };
+
+    xhttp.open('GET', file, false);
+    xhttp.send(null);
+    //if (request.status == 200)
+      //  return request.responseText;
+};
+
+var temp = readJSON('../MarathonResults.json');
+alert(temp);
